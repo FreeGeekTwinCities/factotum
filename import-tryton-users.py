3019a96b4264b059b69374bf03ce866101d399da
Brian Dunnette <brian@dunnette.us>
2012-05-23 10:10:55 -0500

    adding preliminary script to import users from Tryton's Postgres DB into CouchDB

---
+++ import-tryton-users.py
@@ -0,0 +1,27 @@
+import psycopg2
+import urllib2
+import json
+
+# Try to connect
+try:
+    conn=psycopg2.connect("dbname='foo' user='dbuser' password='mypass'")
+except:
+    print "I am unable to connect to the database."
+
+cur = conn.cursor()
+try:
+    cur.execute("""SELECT * from bar""")
+except:
+    print "I can't SELECT from bar"
+
+rows = cur.fetchall()
+print "\nRows: \n"
+for row in rows:
+    print "   ", row[1]
+    username = "dunn0172"
+    userid = "org.couchdb.user:" + username
+    password = "foo"
+    salt = "bar"
+    password_salted = "-hashed-" + password + "," + salt
+    jdata = json.dumps({"type":"user", "roles":[], "_id":%s, "name":username, "password":password_salted}) % (userid, username, password_salted)
+    urllib2.urlopen("http://localhost:5984/frontdesk/_bulk_docs", jdata)
