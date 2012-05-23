a5233e4d28fa221970820c338d7a2e61a52d05d8
Brian Dunnette <brian@dunnette.us>
2012-05-23 15:21:55 -0500

    adding demo of 'typeahead' for volunteer selection on timesheets; removing broken edit link from transactions; also, replicating the users db was a BAD idea

--- replicate-users.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -H 'Content-Type: application/json' -X POST http://localhost:5984/_replicate -d ' {"source": "_users", "target": "frontdesk", "continuous": true}'
-curl -H 'Content-Type: application/json' -X POST http://localhost:5984/_replicate -d ' {"source": "frontdesk", "filter":"frontdesk/user", "target": "_users", "continuous": true}'
-
-
