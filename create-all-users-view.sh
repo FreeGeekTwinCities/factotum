613e27c2a65e6f625c848858a39cf6ed0f1bfe64
Brian Dunnette <brian@dunnette.us>
2012-05-31 15:30:49 -0500

    timesheet typeahead now uses Kanso's users.list function, which should work even if CouchDB is secured (no Admin Party); moving scripts to subfolder

--- create-all-users-view.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -H 'Content-Type: application/json' -X POST http://localhost:5984/_users -d '{"_id": "_design/_users", "language": "javascript", "views": {"all_users": {"map": "function(doc) {if (doc.type === \"user\") {emit(doc.name,null);}}"}}}'
